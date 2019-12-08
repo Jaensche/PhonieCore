1f75ed38fdd5aca92e7fcc7ed8da61969e901bc7
Jan <jan.ertlmeier@gmail.com>
2019-12-08 17:32:06 +0100

    Added missing files

---
+++ PhonieCore/PhonieWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace PhonieCore
+{
+    public class PhonieWorker : BackgroundService
+    {
+        private readonly ILogger<PhonieWorker> _logger;
+
+        public PhonieWorker(ILogger<PhonieWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Worker running at: {DateTime.Now}");
+            await Task.Factory.StartNew(x => { new Radio(); }, new object(), stoppingToken);
+        }
+    }
+}
